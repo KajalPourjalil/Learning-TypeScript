57533c84d2e47b5548e9041f13eff620585c404a
Kajal Pourjalil <rina.skyler@yahoo.com>
2023-05-26 13:12:50 +0300

    working with typescript

--- jscript.js
+++ jscript.js
@@ -30,9 +30,21 @@ const firstInput = document.querySelector('#first-input')
 const secondInput = document.querySelector('#second-input')
 const screen = document.querySelector('.screen')
 
+// function addNumbers(a, b) {
+//     console.log(a + b)
+//     if (typeof a === 'number' && typeof b === 'number') {
+//         screen.innerHTML = a + b;
+//     }
+//     else screen.innerHTML = parseInt(a) + parseInt(b)
+// }
+
+
+//only allowing numbers in the function by using types in typescript
 function addNumbers(a, b) {
-    console.log(a + b)
     screen.innerHTML = a + b
+    
 }
 
 button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
+
+
--- script.ts
+++ script.ts
@@ -1,1 +1,17 @@
-const a = "1"
+let age: number = 34
+let firstName: string = 'lk'
+let busy: boolean = true
+
+
+const button = document.querySelector('.button')
+const firstInput = document.querySelector('#first-input') as HTMLInputElement 
+const secondInput = document.querySelector('#second-input') as HTMLInputElement
+export const screen = document.querySelector('.screen')
+
+function addNumbers(a: number, b: number) {
+    screen.innerHTML = a + b
+    
+}
+
+button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
+
